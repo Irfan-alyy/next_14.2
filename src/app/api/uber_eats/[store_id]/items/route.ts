d7e9e7b5d50fab1cfc110b57cd409b47e1fb3a0a
Ali Irfan <irfanserboon562@gmail.com>
2025-10-02 02:31:28 +0500

    feat: remove cache of menu api to fetch latest menu every time when called

--- src/app/api/uber_eats/[store_id]/items/route.ts
+++ src/app/api/uber_eats/[store_id]/items/route.ts
@@ -1,9 +1,25 @@
 import { uberFetch } from "@/lib/uber";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(_req:NextRequest, { params }: { params: { store_id: string } }){
-    const {store_id}= params
-    // console.log("store id",store_id);
-    const menu= await uberFetch(`/v2/eats/stores/${store_id}/menus`)
-    return NextResponse.json({ menu})
+export async function GET(
+  _req: NextRequest,
+  { params }: { params: { store_id: string } }
+) {
+  const { store_id } = params;
+  // console.log("store id",store_id);
+  const res = await fetch(
+    `${process.env.UBER_API_BASE}/v2/eats/stores/${store_id}/menus`,
+    {
+      cache: "no-store",
+      headers: {
+        Authorization: `Bearer ${process.env.UBER_ACCESS_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const menu=await res.json()
+//   console.log("new menu", menu);
+  return NextResponse.json({ menu });
 }
+
+
